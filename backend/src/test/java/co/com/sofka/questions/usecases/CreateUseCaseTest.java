7dc12e861edf23dd8672572b827415a566d47849
Roxor <junyperez97@gmail.com>
2022-01-10 12:16:42 -0500

    se creo las pruebas de createUseCase y DeleteUseCase

--- backend/src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
+++ backend/src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
@@ -1,7 +1,42 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.repositories.QuestionRepository;
+import co.com.sofka.questions.useCases.CreateUseCase;
+import co.com.sofka.questions.utils.Category;
+import co.com.sofka.questions.utils.Type;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import static org.mockito.Mockito.when;
 
+import java.util.Objects;
+
+@SpringBootTest
 class CreateUseCaseTest {
+    @SpyBean
+    private CreateUseCase createUseCase;
 
+    @MockBean
+    private QuestionRepository repository;
+
+    @Test
+    void createUseCaseTest() {
+
+        var questionDTO = new QuestionDTO("253545", "657", "¿quien creo java?", Type.OPEN, Category.SOFTWARE_DEVELOPMENT);
+
+        var question = new Question();
+
+        when(repository.save(Mockito.any(Question.class))).thenReturn(Mono.just(question));
+
+        var result = createUseCase.apply(questionDTO);
+
+        Assertions.assertEquals(Objects.requireNonNull(result.block()),"253545");
+    }
 }
