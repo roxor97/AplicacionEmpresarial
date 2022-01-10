227c2e7143fc01a2f0399f3863de2bb456d490bd
Roxor <junyperez97@gmail.com>
2022-01-10 11:12:16 -0500

    cambios

--- backend/src/main/java/co/com/sofka/questions/usecases/ListUseCase.java
+++ backend/src/main/java/co/com/sofka/questions/usecases/ListUseCase.java
@@ -16,35 +16,19 @@ import com.mongodb.Function;
 @Validated
 public class ListUseCase implements Supplier<Flux<QuestionDTO>> {
     private final QuestionRepository questionRepository;
-    private final AnswerRepository answerRepository;
     private final MapperUtils mapperUtils;
 
-    public ListUseCase(MapperUtils mapperUtils, QuestionRepository questionRepository, AnswerRepository answerRepository) {
+    public ListUseCase(MapperUtils mapperUtils, QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
         this.mapperUtils = mapperUtils;
-        this.answerRepository = answerRepository;
-    }
-
-    
-
-    private Function<QuestionDTO, Mono<QuestionDTO>> mapQuestionAggregate() {
-        return questionDTO ->
-                Mono.just(questionDTO).zipWith(
-                        answerRepository.findAllByQuestionId(questionDTO.getId())
-                                .map(mapperUtils.mapEntityToAnswer())
-                                .collectList(),
-                        (question, answers) -> {
-                            question.setAnswers(answers);
-                            return question;
-                        }
-                );
     }
 
     @Override
     public Flux<QuestionDTO> get() {
         return questionRepository.findAll()
-                .map(mapperUtils.mapEntityToQuestion())
-                .flatMap(mapQuestionAggregate());
+                .map(mapperUtils.mapEntityToQuestion());
     }
+
+
 
 }
