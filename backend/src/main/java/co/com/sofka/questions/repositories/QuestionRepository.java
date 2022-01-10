c9be9d40c349153eb7b2ed819edccecd1eec99a1
Roxor <junyperez97@gmail.com>
2022-01-10 12:37:24 -0500

    se agrego el test de buscar por categoria

--- backend/src/main/java/co/com/sofka/questions/repositories/QuestionRepository.java
+++ backend/src/main/java/co/com/sofka/questions/repositories/QuestionRepository.java
@@ -1,6 +1,8 @@
 package co.com.sofka.questions.repositories;
 
 import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.utils.Category;
+
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -8,5 +10,5 @@ import reactor.core.publisher.Flux;
 @Repository
 public interface QuestionRepository extends ReactiveCrudRepository<Question, String> {
     Flux<Question> findByUserId(String userId);
-    Flux<Question> findAllByCategory(String category);
+    Flux<Question> findAllByCategory(String string);
 }
---
+++ backend/src/test/java/co/com/sofka/questions/usecases/FindAllByCategoryUseCaseTest.java
@@ -0,0 +1,56 @@
+package co.com.sofka.questions.usecases;
+
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.repositories.QuestionRepository;
+import co.com.sofka.questions.useCases.FindAllByCategoryUseCase;
+import co.com.sofka.questions.utils.Category;
+import co.com.sofka.questions.utils.MapperUtils;
+import co.com.sofka.questions.utils.Type;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class FindAllByCategoryUseCaseTest {
+
+    @MockBean
+    QuestionRepository repository;
+
+    @SpyBean
+    FindAllByCategoryUseCase findAllByCategory;
+
+    @BeforeEach
+    public void setup(){
+        MapperUtils mapperUtils = new MapperUtils();
+        repository = mock(QuestionRepository.class);
+        findAllByCategory = new FindAllByCategoryUseCase(mapperUtils, repository);
+    }
+
+    @Test
+    void findAllByCategoryUseCaseTest() {
+        var question = new QuestionDTO("253545", "657", "¿quien creo java?", Type.OPEN, Category.SOFTWARE_DEVELOPMENT);
+        when(repository.findAllByCategory("SOFTWARE_DEVELOPMENT"))
+                .thenReturn(Flux.just(question));
+
+        StepVerifier.create(findAllByCategory.apply("SOFTWARE_DEVELOPMENT"))
+                .expectNextMatches(questionDTO -> {
+                    assert questionDTO.getUserId().equals("657");
+                    assert questionDTO.getCategory().equals(Category.SOFTWARE_DEVELOPMENT);
+                    assert questionDTO.getQuestion().equals("¿quien creo java?");
+                    assert questionDTO.getType().equals(Type.OPEN);
+                    return true;
+                })
+                .verifyComplete();
+
+        verify(repository).findAllByCategory("SOFTWARE_DEVELOPMENT");
+    }
+}
