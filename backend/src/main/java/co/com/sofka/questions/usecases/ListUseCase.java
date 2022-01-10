350afaa6e7619dbcae275d19200bd7531a1807b5
Roxor <junyperez97@gmail.com>
2022-01-10 11:02:00 -0500

    se agrego el getAll

--- backend/src/main/java/co/com/sofka/questions/usecases/ListUseCase.java
+++ backend/src/main/java/co/com/sofka/questions/usecases/ListUseCase.java
@@ -1,28 +1,50 @@
 package co.com.sofka.questions.usecases;
 
 import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
 import co.com.sofka.questions.reposioties.QuestionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.function.Supplier;
+
+import com.mongodb.Function;
 
 @Service
 @Validated
 public class ListUseCase implements Supplier<Flux<QuestionDTO>> {
     private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
     private final MapperUtils mapperUtils;
 
-    public ListUseCase(MapperUtils mapperUtils, QuestionRepository questionRepository) {
+    public ListUseCase(MapperUtils mapperUtils, QuestionRepository questionRepository, AnswerRepository answerRepository) {
         this.questionRepository = questionRepository;
         this.mapperUtils = mapperUtils;
+        this.answerRepository = answerRepository;
+    }
+
+    
+
+    private Function<QuestionDTO, Mono<QuestionDTO>> mapQuestionAggregate() {
+        return questionDTO ->
+                Mono.just(questionDTO).zipWith(
+                        answerRepository.findAllByQuestionId(questionDTO.getId())
+                                .map(mapperUtils.mapEntityToAnswer())
+                                .collectList(),
+                        (question, answers) -> {
+                            question.setAnswers(answers);
+                            return question;
+                        }
+                );
     }
 
     @Override
     public Flux<QuestionDTO> get() {
         return questionRepository.findAll()
-                .map(mapperUtils.mapEntityToQuestion());
+                .map(mapperUtils.mapEntityToQuestion())
+                .flatMap(mapQuestionAggregate());
     }
 
 }
