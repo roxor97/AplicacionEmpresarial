86c68d38b9a15dd7f650b22416a39ad569529b81
Roxor <junyperez97@gmail.com>
2022-01-10 19:41:19 -0500

    se adapto el componente answer

--- frontend/src/components/Answer.js
+++ frontend/src/components/Answer.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteAnswer } from "../actions/questionActions";
+import Swal from "sweetalert2";
 
-export const Answer = ({ answer }) => (
-  <aside className="answer">
-    <p>{answer.answer}</p>
-  </aside>
-)
+const Answer = ({answer, log}) => {
+
+  const dispatch = useDispatch()
+
+  const onDeleteAnswer = (id) => {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "Una vez eliminada no podras recuperar la respuesta!!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteAnswer(id, answer.questionId));
+      }
+    })
+  };
+
+  return ( <aside className="answer">
+  <p>{answer.answer}</p>
+  {
+    log === answer.userId && < button className="button right" onClick={() => onDeleteAnswer(answer.id)}>
+      DELETE
+    </button>
+  }
+</aside> );
+}
+ 
+export default Answer;
